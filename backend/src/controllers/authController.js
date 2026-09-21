ecb38fc0ba69fd16c52461d6de4f90d784657289
abdulrinshad <arinshad0@gmail.com>
2026-09-21 12:53:01 +0530

    change user.js to User.js

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/User");
 
 // @desc    Register a new user
 // @route   POST /api/auth/register
